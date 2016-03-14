123b5029b32064f82a59db35bd5ac04a5685f27c
Andreas Böhrnsen <andreas@frontrunner.io>
2016-03-14 20:53:04 +0100

    add livereload with gulp

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var kss         = require('kss');
+var sass        = require('gulp-sass');
+var sourcemaps  = require('gulp-sourcemaps');
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['styleguide', 'sass'], function() {
+  browserSync.init({
+    server: "./"
+  });
+
+  gulp.watch("src/**/*.scss", ['styleguide', 'sass']);
+  gulp.watch("src/**/*.md").on('change', browserSync.reload);
+  gulp.watch("styleguide/*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+  return gulp.src('./src/**/*.scss')
+  .pipe(sourcemaps.init())
+  .pipe(sass().on('error', sass.logError))
+  .pipe(sourcemaps.write('./dist'))
+  .pipe(gulp.dest('./dist/'))
+  .pipe(browserSync.stream());
+});
+
+gulp.task('styleguide', function(cb) {
+  kss({
+    "css": ["../dist/styleguide.css"],
+    "destination":  "styleguide",
+    "source": ["src"],
+    "title": "Node Style Guide"
+  }, cb);
+});
+
+gulp.task('default', ['serve']);
